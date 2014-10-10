c241d0317a36adc10ba522f6dc7da43aad302c85
Elusivehawk <be_the_trade@yahoo.com>
2014-10-10 01:53:24 -0700

    Expanded TaskURLRequest.

--- src/com/elusivehawk/util/task/TaskURLRequest.java
+++ src/com/elusivehawk/util/task/TaskURLRequest.java
@@ -3,7 +3,10 @@ package com.elusivehawk.util.task;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 import com.elusivehawk.util.io.ByteStreams;
+import com.elusivehawk.util.storage.Pair;
+import com.google.common.collect.Lists;
 
 /**
  * 
@@ -13,7 +16,10 @@ import com.elusivehawk.util.io.ByteStreams;
  */
 public class TaskURLRequest extends TaskURL
 {
-	protected byte[] result = null;
+	private final List<Pair<String>> props = Lists.newArrayList();
+	
+	private int connectTimeout = 15000, readTimeout = 15000;
+	private byte[] result = null;
 	
 	public TaskURLRequest(ITaskListener tlis, URL adr)
 	{
@@ -25,6 +31,15 @@ public class TaskURLRequest extends TaskURL
 	protected boolean finishTask() throws Throwable
 	{
 		URLConnection con = this.url.openConnection();
+		
+		con.setConnectTimeout(this.connectTimeout);
+		con.setReadTimeout(this.readTimeout);
+		
+		for (Pair<String> prop : this.props)
+		{
+			con.setRequestProperty(prop.one, prop.two);
+			
+		}
 		
 		con.connect();
 		
@@ -41,9 +56,30 @@ public class TaskURLRequest extends TaskURL
 		return true;
 	}
 	
-	
 	public byte[] getResult()
 	{
 		return this.result;
 	}
+	
+	public TaskURLRequest setConnectionTimeout(int ct)
+	{
+		this.connectTimeout = ct;
+		
+		return this;
+	}
+	
+	public TaskURLRequest setReadTimeout(int rt)
+	{
+		this.readTimeout = rt;
+		
+		return this;
+	}
+	
+	public TaskURLRequest addRequestProp(String name, String value)
+	{
+		this.props.add(Pair.createPair(name, value));
+		
+		return this;
+	}
+	
 }
