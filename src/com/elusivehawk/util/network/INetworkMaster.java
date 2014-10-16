85bb6503244ce6d0679aef60c8fec9d52ad855b4
Elusivehawk <be_the_trade@yahoo.com>
2014-10-15 21:52:30 -0700

    Added networking system, from the Caelum Engine.

---
+++ src/com/elusivehawk/util/network/INetworkMaster.java
@@ -0,0 +1,16 @@
+
+package com.elusivehawk.util.network;
+
+/**
+ * 
+ * The core interface for networking; Implement this to your game instance, or a dedicated networking manager.
+ * 
+ * @author Elusivehawk
+ */
+public interface INetworkMaster
+{
+	int getEncryptionBitCount();
+	
+	boolean handshake(Connection connection, Packet pkt);
+	
+}
